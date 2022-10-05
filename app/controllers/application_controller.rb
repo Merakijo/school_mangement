aaf312905b27588c4f0df3c63af291b239208a4b
merakijo <vikashjoshi431@gmail.com>
2022-10-05 18:02:34 +0530

    recent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,35 @@
 class ApplicationController < ActionController::Base
+
+	 # before_action :require_user
 	
 	helper_method :current_user, :logged_in?
-	def current_user
-		@current_user ||= Student.find(session[:student_id]) if session[:student_id]
+	
+	def current_user  
+		# details of the user who is logges in
+		if session[:student_id]
+			Student.find(session[:student_id])
+		# else session[:teacher_id]
+		# 	Teacher.find(session[:teacher_id])
+		end
+		
+			# @current_user ||= Student.find(session[:student_id] if session[:student_id]
+   		
+   			# @current_user ||= Teacher.find(session[:teacher_id]) if session[:teacher_id]
+   	
     end	
+    #Memoization ||= is used:  i.e it wil only hit the database if the current_user is empty 
 
 	def logged_in?
 		!!current_user
+		# !! i.e make it boolean for true/false value
+	end
+
+	def require_user
+		if !logged_in?
+			flash[:notice] = "Login karle pehle"
+			redirect_to login_path
+		end
 	end
 
 end
+ 
